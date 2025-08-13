9be7dda01bedaf121f07348f24d34e20f47d98f2
Carolina Lee <111212023+roxo-1@users.noreply.github.com>
2025-08-13 10:42:50 -0300

    COMMIT MAIN

---
+++ MergeSort.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class Main{
+    public static void main(String[] args){
+        int []v1 = {1,3,5,7,9};
+        int []v2 = {2,4,6,8,10};
+        int []v3 = intercala(v1, v2);
+        System.out.println(Arrays.toString(v3));
+    }
+    private static int[] intercala(int[] v1, int[] v2){
+        int []v3 = new int[v1.length + v2.length];
+        for(int i = 0, j=0; i<v1.length; i++, j+=2){
+            v3[j] = v1[i];
+            v3[j+1] = v2[i];
+        }
+        return v3;
+    }
+}
