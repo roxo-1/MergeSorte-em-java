e0ac46888fee26b783d0c266af52fa063fb59c4f
Carolina Lee <111212023+roxo-1@users.noreply.github.com>
2025-08-18 07:40:52 -0300

    Update MergeSort.java

--- MergeSort.java
+++ MergeSort.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 
-public class Main{
+public class MergeSort{
     public static void main(String[] args){
         int []v1 = {1,3,5,7,9};
         int []v2 = {2,4,6,8,10};
@@ -15,4 +15,5 @@ public class Main{
         }
         return v3;
     }
+
 }
